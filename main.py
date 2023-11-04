0f0d051e0b2e7076ec05d6993e7ab24406d5706c
Claude FOTSO <fotso1claude@gmail.com>
2023-11-04 22:05:58 +0100

    Adele Test

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+import collections
+import json
+
 import requests
 from pprint import pprint
 
 from bs4 import BeautifulSoup
 
 
-def extract_lyrics(url: str):
+def extract_lyrics(url: str, word_length=5 ):
     r = requests.get(url)
     if r.status_code != 200:
         print("Unable to load the page")
         return []
+    print(f"Fetching url: {url}")
     soup = BeautifulSoup(r.content, "html.parser")
     lyrics = soup.find_all("div", class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL")
     lyrics = [item.stripped_strings for item in lyrics]
@@ -17,17 +21,19 @@ def extract_lyrics(url: str):
     all_words = []
     for elt in lyrics:
         for sentence in elt:
-            all_words.extend([word.strip(",").strip(".") for word in sentence if len(word) > 2])
+            all_words.extend([word.strip(",").strip(".").lower() for word in sentence if
+                              len(word) > word_length and "[" not in word and "]" not in word])
 
-    pprint(all_words)
+    return all_words
 
 
 def get_all_urls():
     page_number = 1
     links = []
     while True:
-        r = requests.get(f"https://genius.com/api/artists/29743/songs?page={page_number}&sort=popularity")
+        r = requests.get(f"https://genius.com/api/artists/2300/songs?page={page_number}&sort=popularity")
         if r.status_code == 200:
+            print(f"Fetching page {page_number}")
             response = r.json().get("response", {})
             next_page = response.get("next_page")
 
@@ -42,5 +48,20 @@ def get_all_urls():
     return links
 
 
-urls = get_all_urls()
-extract_lyrics(url="https://genius.com/Patrick-bruel-perlimpinpin-lyrics")
+def get_all_words():
+    urls = get_all_urls()
+    words = []
+    for url in urls:
+        lyrics = extract_lyrics(url)
+        words.extend(lyrics)
+
+    with open("datas-adele.json", "w") as f:
+        json.dump(words, f, indent=4)
+
+    counter = collections.Counter(words)
+    most_common_words = counter.most_common(15)
+
+    pprint(most_common_words)
+
+
+get_all_words()
