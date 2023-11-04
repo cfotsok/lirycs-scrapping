632c9aeff56b6bbfab0f21e5d18627d7d2f2f3b5
Claude FOTSO <fotso1claude@gmail.com>
2023-11-04 20:05:24 +0100

    Get the word of a song in a list

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 import requests
+from pprint import pprint
+
+from bs4 import BeautifulSoup
+
+
+def extract_lyrics(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        print("Unable to load the page")
+        return []
+    soup = BeautifulSoup(r.content, "html.parser")
+    lyrics = soup.find_all("div", class_="Lyrics__Container-sc-1ynbvzw-1 kUgSbL")
+    lyrics = [item.stripped_strings for item in lyrics]
+
+    lyrics = [[elt.split() for elt in item] for item in lyrics]
+    words = []
+    for elt in lyrics:
+        for sentence in elt:
+            words.extend(sentence)
+
+    pprint(words)
+
 
 def get_all_urls():
-  r = requests.get("https://genius.com/api/artists/29743/songs?page=1 &sort=popularity")
+    page_number = 1
+    links = []
+    while True:
+        r = requests.get(f"https://genius.com/api/artists/29743/songs?page={page_number}&sort=popularity")
+        if r.status_code == 200:
+            response = r.json().get("response", {})
+            next_page = response.get("next_page")
+
+            songs = response.get("songs")
+            links.extend([song.get("url") for song in songs])
+
+            page_number += 1
+
+            if not next_page:
+                print("No more pages to fetch")
+                break
+    return links
+
+
+urls = get_all_urls()
+extract_lyrics(url="https://genius.com/Patrick-bruel-perlimpinpin-lyrics")
